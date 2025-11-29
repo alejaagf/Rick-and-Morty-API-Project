241437290be85281ae464177c5462fde34cde5a2
alejaagf <alejita_-galindo@hotmail.com>
2025-11-28 21:42:41 -0500

    feat: Se implemento el switch de modo claro/oscuro

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ async function getMortyCharacters(options = { forceRefresh: false }) {
     const CACHE_KEY = "rm_characters_v1";
     const CACHE_TTL = 1000 * 60 * 60; // 1 hora
 
+
     // intentar leer caché
     try {
         const raw = localStorage.getItem(CACHE_KEY);
@@ -106,3 +107,19 @@ async function start() {
 }
 
 start();
+
+let toggle = document.getElementById('toggle');
+let label_toggle = document.getElementById('label_toggle');
+toggle.addEventListener('change',(event) => {
+    let checked = event.target.checked;
+    document.body.classList.toggle('dark');
+
+    if (checked==true){
+        label_toggle.innerHTML='<i class="fa-sharp fa-solid fa-sun"></i>'
+        label_toggle.style.color= '#ffff00';
+    }else {
+        label_toggle.innerHTML='<i class="fa-sharp fa-solid fa-moon"></i>'
+        label_toggle.style.color= '#15153b';
+    }
+    
+})
